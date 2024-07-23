03bd8b4bd37a995bf214b802bde94e2c1df96956
haseeb-iqbal <a.haseeb19@gmail.com>
2024-07-23 15:39:17 +0500

    Made website responsive

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function OpenTab(clickedObject, tabName) {
   clickedObject.classList.add("active-link");
   document.getElementById(tabName).classList.add("active-tab");
 }
+
+let SideMenu = document.getElementById("sidemenu");
+
+function OpenMenu() {
+  SideMenu.style.right = "0";
+}
+
+function CloseMenu() {
+  SideMenu.style.right = "-200px";
+}
